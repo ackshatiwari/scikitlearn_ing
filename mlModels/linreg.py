19883e165f31e09fbeedf4d704ca73baf029de16
Ackshat Tiwari <ackshat.tiwari@gmail.com>
2026-03-29 16:00:39 -0400

    linear_regression w/ scikit-learn

---
+++ mlModels/linreg.py
@@ -0,0 +1,31 @@
+import pandas as pd
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+
+df = pd.read_csv("linreg_housingPrices.csv")
+
+X = df[["size", "rooms"]]
+y = df["price"]
+
+n_test = 20
+
+X_train = X.iloc[:-n_test]
+y_train = y.iloc[:-n_test]
+
+# Test = last 20 rows
+X_test = X.iloc[-n_test:]
+y_test = y.iloc[-n_test:]
+
+model = LinearRegression()
+
+model.fit(X_train, y_train)
+
+prediction = model.predict(X_test)
+print(prediction)
+
+r2 = r2_score(y_test, prediction)
+
+print("R^2:", r2)
+
+
